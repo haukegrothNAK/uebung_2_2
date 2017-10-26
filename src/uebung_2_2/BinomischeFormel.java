dc48f7919851681740b76bbde2790a2b8a789bf1
15497 <15497@A001-11.nordakademie.intern>
2017-10-26 11:05:30 +0200

    Binomische Formel programmiert

---
+++ src/uebung_2_2/BinomischeFormel.java
@@ -0,0 +1,16 @@
+package uebung_2_2;
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double a = 8;
+		double b = 3;
+		
+		double Ergebnis = (a*a+2*a*b+b*b);
+		//double Ergebnis = ((a+b)*(a+b));
+				System.out.println("Ergebnis = "+ Ergebnis);
+	}
+
+}
